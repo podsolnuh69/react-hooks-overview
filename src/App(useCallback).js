907e099f453c156250c6d54a004208e69537273c
podsolnuh69 <kkoiku3@gmail.com>
2022-05-11 09:26:36 +0300

    add useCallback hook

---
+++ src/App(useCallback).js
@@ -0,0 +1,27 @@
+import React, {useState, useCallback} from 'react'
+import ItemsList from './ItemsList'
+
+function App() {
+  const [colored, setColored] = useState(false)
+  const [count, setCount] = useState(1)
+
+  const styles = {
+    color: colored ? 'darkred' : 'black'
+  }
+
+  const generateItemsFromAPI = useCallback((indexNumber) => {
+    return new Array(count).fill('').map((_, i) => `Элемент ${i + indexNumber}`)
+  }, [count])
+
+  return (
+    <>
+      <h1 style={styles}>Количество элементов: {count}</h1>
+      <button onClick={() => setCount(prev => prev + 1)}>Добавить</button>
+      <button onClick={() => setColored(prev => !prev)}>Изменить</button>
+
+      <ItemsList getItems={generateItemsFromAPI} />
+    </>
+  )
+}
+
+export default App
--- src/App.js
+++
@@ -1,5 +0,0 @@
-const App = (props) => {
-    return <h1>Hello</h1>
-}
-
-export default App;
